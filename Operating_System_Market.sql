7e474cf3c3d52c0064884bb637a90524ec449975
Gooyam <ndoan@pdx.edu>
2024-06-23 17:46:52 -0700

    create table

---
+++ Operating_System_Market.sql
@@ -0,0 +1,30 @@
+CREATE TABLE online_data (
+	_record_number int AUTO_INCREMENT NOT NULL,
+    Date varchar(7)
+    Windows numeric(5,2),
+    Android numeric(5,2),
+    iOS numeric(5,2),
+    'OS X' numeric(4,2),
+    Unknown numeric(4,2),
+    Linux numeric(4,2),
+    'Series 40' numeric(4,2),
+    SymbianOS numeric(4,2),
+    Samsung numeric(4,2),
+    'BlackBerry OS' numeric(4,2),
+    'Chrome OS'numeric(4,2),
+    'Nokia Unknown' numeric(4,2),
+    Playstation numeric(4,2),
+    'Sony Ericsson' numeric(4,2),
+    KaiOS numeric(4,2),
+    Xbox numeric(4,2),
+    bada numeric(4,2),
+    Tizen numeric(4,2),
+    LG numeric(4,2),
+    Nintendo numeric(4,2),
+    Other numeric(4,2),
+    Primary key(_record_number)
+    
+-- -------------------------    
+-- Insert records 1 - 185
+-- -------------------------
+
